2c6ae15d4e528beba6ab3d100b32d4030c4b4951
Gppsv143 <97022674+Gppsv143@users.noreply.github.com>
2024-12-16 17:21:17 +0530

    Create Home.js

---
+++ frontend/ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to Praveen Travels</h1>
+      <button>Search Buses</button>
+    </div>
+  );
+};
+
+export default Home;
